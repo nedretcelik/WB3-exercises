0d1cb0b068d2b739ddd28dbc83be1b19f1f7009a
nedretcelik <nedretcelik97@gmail.com>
2024-05-01 11:15:19 -0400

    add function

--- strings/parsing.js
+++ strings/parsing.js
@@ -1,4 +1,4 @@
-let fullName = "Brenda Kaye";
+/* let fullName = "Brenda Kaye";
 
 let spaceIndex = fullName.indexOf(" ");
 
@@ -9,4 +9,27 @@ let lastName = fullName.substring(7);
 
 console.log(`Name: ${fullName}`);
 console.log(`First Name : ${firstName}`);
-console.log(`Last Name:  ${lastName}`);
+console.log(`Last Name:  ${lastName}`); */
+
+
+function parseAndDisplayName(fullName) {
+
+    let spaceIndex = fullName.indexOf(" ");
+
+
+    
+    let firstName = fullName.substring(0,spaceIndex);
+    let lastName = fullName.substring(spaceIndex +1);
+    
+    console.log(`Name: ${fullName}`);
+    console.log(`First Name : ${firstName}`);
+    console.log(`Last Name:  ${lastName}`);
+}
+
+parseAndDisplayName("Brenda Kaye");
+parseAndDisplayName("Ian Auston");
+parseAndDisplayName("Siddalee Grace");
+
+
+
+
