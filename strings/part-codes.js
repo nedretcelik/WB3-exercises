267c0b55d540bff6526edc3a28341a5ce6273f8a
nedretcelik <nedretcelik97@gmail.com>
2024-05-01 13:37:55 -0400

    add new function

--- strings/part-codes.js
+++ strings/part-codes.js
@@ -29,16 +29,31 @@ function getSize(code) {
     let findHyphen = code.indexOf("-");
 
     let productSize = code.substring(findHyphen+1)
- 
-    return productSize;
+
+    if(productSize == "S") {
+        return `the small (${productSize})`
+    } else if (productSize == "M") {
+        return `the medium (${productSize})`
+    } else if (productSize == "L") {
+        return `the Large (${productSize})`
+    } else {
+
+        return `the size (${productSize})`
+    }
+
+
 
 }
 
+
+
+
+
 // getSize("ACME:123-L") // L
 
-console.log(`the large ${getSize("ACME:123-L")} part ${getProductNumber("ACME:123-L")} is supplied by ${getSupplier("ACME:123-L")}`);
-console.log(`the large ${getSize("DI:12345-M")} part ${getProductNumber("DI:12345-M")} is supplied by ${getSupplier("DI:12345-M")}`);
-console.log(`the large ${getSize("ACE:1-12")} part ${getProductNumber("ACE:1-12")} is supplied by ${getSupplier("ACE:1-12")}`);
+console.log(`${getSize("ACME:123-L")} part ${getProductNumber("ACME:123-L")} is supplied by ${getSupplier("ACME:123-L")}`);
+console.log(`${getSize("DI:12345-M")} part ${getProductNumber("DI:12345-M")} is supplied by ${getSupplier("DI:12345-M")}`);
+console.log(`${getSize("ACE:1-12")} part ${getProductNumber("ACE:1-12")} is supplied by ${getSupplier("ACE:1-12")}`);
 
 
 
