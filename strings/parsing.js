0069c9aa5f90697d74327af4eaa994b317c3484c
nedretcelik <nedretcelik97@gmail.com>
2024-05-01 11:06:09 -0400

    string exercises

---
+++ strings/parsing.js
@@ -0,0 +1,12 @@
+let fullName = "Brenda Kaye";
+
+let spaceIndex = fullName.indexOf(" ");
+
+// console.log(spaceIndex);
+
+let firstName = fullName.substring(0,6);
+let lastName = fullName.substring(7);
+
+console.log(`Name: ${fullName}`);
+console.log(`First Name : ${firstName}`);
+console.log(`Last Name:  ${lastName}`);
